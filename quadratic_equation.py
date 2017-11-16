396b5d1aeecc6a7928f9dc7650c8b3968b527caf
papersaltserver <noconnect@gmail.com>
2017-11-16 09:01:49 +0300

    created get_first_root function

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,14 +1,15 @@
 from math import sqrt
 
+def get_first_root(a, b, discriminant):
+    return (-b - sqrt(discriminant)) / (2 * a)
 
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
     if discriminant == 0:
-        root1 = (-b - sqrt(discriminant)) / (2 * a)
-        root2 = (-b + sqrt(discriminant)) / (2 * a)
+        root1 = get_first_root(a, b, discriminant)
         return root1, None
     elif discriminant > 0:
-        root1 = (-b - sqrt(discriminant)) / (2 * a)
+        root1 = get_first_root(a, b, discriminant)
         root2 = (-b + sqrt(discriminant)) / (2 * a)
         return root1, root2
     elif discriminant < 0:
