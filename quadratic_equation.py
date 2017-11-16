621591e3f3cd982ea8c702fa1fb27bed20315986
Dmitry Trukhanov <noconnect@gmail.com>
2017-11-16 18:00:09 +0300

    Changed root2 calculations

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -4,10 +4,7 @@ def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
     if discriminant >= 0:
         root1 = (-b - sqrt(discriminant)) / (2 * a)
-        if discriminant == 0:
-            return root1, None
-        elif discriminant > 0:
-            root2 = (-b + sqrt(discriminant)) / (2 * a)
-            return root1, root2
+        root2 = (-b + sqrt(discriminant)) / (2 * a) if discriminant > 0 else None
+        return root1, root2
     else:
         return None, None
