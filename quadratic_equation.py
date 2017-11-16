4cdc42713a50f939ee5c05ea91dcd87acc0a3513
Dmitry Trukhanov <noconnect@gmail.com>
2017-11-16 16:35:51 +0300

    Changed to multiple if

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,17 +1,13 @@
 from math import sqrt
 
-def get_first_root(a, b, discriminant):
-    first_root = (-b - sqrt(discriminant)) / (2 * a)
-    return first_root
-
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
-    if discriminant == 0:
-        root1 = get_first_root(a, b, discriminant)
-        return root1, None
-    elif discriminant > 0:
-        root1 = get_first_root(a, b, discriminant)
-        root2 = (-b + sqrt(discriminant)) / (2 * a)
-        return root1, root2
-    elif discriminant < 0:
+    if discriminant >= 0:
+        root1 = (-b - sqrt(discriminant)) / (2 * a)
+        if discriminant == 0:
+            return root1, None
+        elif discriminant > 0:
+            root2 = (-b + sqrt(discriminant)) / (2 * a)
+            return root1, root2
+    else:
         return None, None
